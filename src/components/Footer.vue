26b9cba09a891dbed2780e060354d578a786d6fb
Parth Sarthee <14sarthi@gmail.com>
2023-04-09 16:01:12 +0530

    updated footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,11 +1,37 @@
 <template>
-    <div id="contact" class="py-16 px-4 bg-stone-900 text-white  ">
-        <div class="max-w-5xl mx-auto">
-            <h1 class="text-6xl uppercase mb-12">Contact Us</h1>
-            <div class="flex flex-wrap justify-center gap-12">
-                <div class="flex flex-col w-72 justify-evenly">
-                    <h1 class="text-lg font-medium">Reach Out To Us</h1>
-                    <div>
+    <div id="contact" class="lg:h-screen flex flex-row justify-center items-center bg-black text-white  ">
+        <div class="lg:max-w-5xl rounded flex flex-col lg:flex-row justify-center items-center bg-gray-800 lg:m-8">
+
+            <div class="flex flex-col lg:w-1/2 gap-6 p-8 lg:border-r lg:border-gray-500">
+
+                <h1 class="text-4xl font-semibold text-indigo-500">CONTACT US</h1>
+                <form class="flex flex-col gap-6">
+                    <div class="flex md:flex-row flex-col gap-4">
+                        <div class="flex flex-col">
+                            <label for="name" class="mb-2">Name</label>
+                            <input type="text" name="name" id="name" class="bg-gray-700 text-white py-1.5 px-2 border border-gray-500" placeholder="Full Name">
+                        </div>
+                        <div class="flex flex-col">
+                            <label for="email" class="mb-2">Email</label>
+                            <input type="email" name="email" id="email" class="bg-gray-700 text-white py-1.5 px-2 border border-gray-500" placeholder="mail@domain.com">
+                        </div>
+                    </div>
+                    <div class="flex flex-col">
+                        <label for="message" class="mb-2">Message</label>
+                        <textarea name="message" cols="30" rows="10" class="bg-gray-700 text-white p-2 border border-gray-500" placeholder="Enter your message here"></textarea>
+                    </div>
+
+                    <div class="flex flex-row">
+                        <button class=" text-white w-full p-2 font-medium bg-indigo-500 rounded">SEND MESSAGE</button>
+                    </div>
+
+                </form>
+            </div>
+
+            <div class="flex flex-col lg:w-1/2 gap-6 p-8 justify-center items-center">
+                <div class="flex flex-col gap-3">
+                    <h1 class="text-4xl font-semibold text-indigo-500">CONTACT INFO</h1>
+                    <div class="max-w-md">
                         <i class=" mr-2 fa-solid fa-location-dot"></i>
                         <span>Dwarka Sector 9, Institutional Area, Near Dwarka Sec. 10, Metro Station. New Delhi-110075</span>
                     </div>
@@ -26,9 +52,10 @@
                         <span>9315911714</span>
                     </div>
                 </div>
-                <div class="flex flex-col w-72 justify-evenly">
-                    <h1 class="text-lg font-medium">Important Links</h1>
-                    <div>
+
+                <div class="flex flex-col gap-3">
+                    <h1 class="text-4xl font-semibold text-indigo-500">FOLLOW US</h1>
+                    <div class="max-w-md">
                         <i class=" mr-2 fa-solid fa-location-dot"></i>
                         <span>Dwarka Sector 9, Institutional Area, Near Dwarka Sec. 10, Metro Station. New Delhi-110075</span>
                     </div>
@@ -48,20 +75,11 @@
                         <i class="mr-2 fa-solid fa-mobile-screen-button"></i>
                         <span>9315911714</span>
                     </div>
-                </div>
-                <div class="flex flex-col gap-3 justify-center">
-                    <input type="text" placeholder="Name">
-                    <input type="email" placeholder="Email">
-                    <textarea placeholder="Message" cols="30" rows="3"></textarea>
-                    <button class="bg-indigo-500 p-2 rounded uppercase font-semibold">Submit</button>
                 </div>
             </div>
+
         </div>
     </div>
 </template>
 
-<style scoped lang="postcss">
-input, textarea{
-    @apply p-2 rounded text-white bg-slate-700;
-}
-</style>
+<style src="@vueform/multiselect/themes/default.css"></style>
